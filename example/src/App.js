1d30405b990fc03e8551d0a991774737b66b5101
maitrucquynh111 <maitrucquynh111@gmail.com>
2019-08-20 17:02:46 +0700

    update carousel

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
-
+import branch1 from './images/branch_01.jpg'
+import branch2 from './images/branch_02.jpg'
+import branch3 from './images/branch_03.jpg'
+import branch4 from './images/branch_04.jpg'
 import ExampleComponent from 'masspa-carousel'
-
+const listBranches = [
+  {
+    branchName: 'Chi nhánh Gia Lai',
+    imageUrl: branch1,
+  },
+  {
+    branchName: 'Chi nhánh Gia Lai',
+    imageUrl: branch2,
+  },
+  {
+    branchName: 'Chi nhánh Gia Lai',
+    imageUrl: branch3,
+  },
+  {
+    branchName: 'Chi nhánh Gia Lai',
+    imageUrl: branch4,
+  },
+]
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent
+          type="slideThree"
+          listBranches={listBranches}
+        />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,75 @@
 import React, { Component } from 'react'
+import Slider from "react-slick"
 import PropTypes from 'prop-types'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import './styles.css'
 
-import styles from './styles.css'
-
+const styleSlideDefault = {
+  dots: true,
+  lazyLoad: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  className: "center",
+  centerMode: true,
+  centerPadding: "40px",
+  autoplay: true,
+  autoplaySpeed: 2000,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        centerMode: false,
+        centerPadding: "0px",
+        lazyLoad: false,
+        fade: true,
+      },
+    },
+  ],
+}
+const styleSlideOne = {
+  dots: false,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  pauseOnHover: false,
+}
+const styleSlideThree = {
+  dots: true,
+  lazyLoad: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  className: "center",
+  // centerMode: true,
+  centerPadding: "40px",
+  autoplay: true,
+  autoplaySpeed: 2000,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        centerMode: false,
+        centerPadding: "0px",
+        lazyLoad: false,
+        fade: true,
+      },
+    },
+  ],
+}
 export default class ExampleComponent extends Component {
   static propTypes = {
     text: PropTypes.string
@@ -10,12 +77,32 @@ export default class ExampleComponent extends Component {
 
   render() {
     const {
-      text
+      listBranches,
+      type
     } = this.props
-
+    let styleCarousel;
+    if(type == "slideOne"){
+      styleCarousel = styleSlideOne;
+    }
+    else if(type == "slideThree"){
+      styleCarousel = styleSlideThree;
+    }
+    else{
+      styleCarousel = styleSlideDefault
+    }
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div>
+        <Slider {...styleCarousel}>
+          {listBranches.map((item, index) => {
+            return(
+              <div>
+                <div className="branch-img">
+                  <img alt={item.branchName} src={item.imageUrl}/>
+                </div>
+              </div>
+            )
+          })}
+        </Slider>
       </div>
     )
   }
